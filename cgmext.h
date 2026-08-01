e65bfeeaff0016db92ecedf070b0fd9dbb8b9987
Rei <bassherobae@gmail.com>
2026-08-01 16:32:55 +0800

    update: cgmext and the cgmext.h header refactors

--- cgmext.h
+++ cgmext.h
@@ -1,57 +1,19 @@
-#ifndef CGEMXT_H
-#define CGEMXT_H
+#ifndef CGMEXT_H
+#define CGMEXT_H
 
-#define CGEMXT_VER "0.0.1"
+#define CGMEXT_VER "0.0.1"
 
 /* Includes */
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-/* Defintions */
+/* Definitions */
 #ifdef _WIN32
 #define YYEXPORT __declspec(dllexport)
 #else
 #define YYEXPORT __attribute__((visibility("default")))
-#endif
-
-/* Helper types */
-#ifdef CE_EASY
-
-typedef double gm_bool;
-
-#define gm_true 1.0
-#define gm_false 0.0
-
-typedef const char *string;
-typedef double real;
-typedef int64_t int64;
-
-typedef void *pointer_any;
-
-typedef void *buffer_any;
-
-typedef int8_t *buffer_s8;
-typedef uint8_t *buffer_u8;
-typedef int16_t *buffer_s16;
-typedef uint16_t *buffer_u16;
-typedef int32_t *buffer_s32;
-typedef uint32_t *buffer_u32;
-
-typedef float *buffer_f32;
-typedef double *buffer_f64;
-
-#ifdef CE_SPECIAL_BUFS
-/* NOTE: These types are NOT guaranteed in C lang! */
-typedef _Float16 *buffer_f16;
-typedef _Float128 *buffer_f128;
-#endif
-
-typedef bool *buffer_bool;
-
-typedef const char *buffer_string;
-typedef char *buffer_text; /* err... */
-
 #endif
 
 /* Helper functions */
